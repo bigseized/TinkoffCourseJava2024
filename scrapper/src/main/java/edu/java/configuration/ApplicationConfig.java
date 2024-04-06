404ecfa320623b1ba0419e810399fbf6a5cbc0e5
Михаил Беляев <mishkapnz@mail.ru>
2024-04-06 21:51:55 +0300

    [hw7] modify: delete old library code

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -2,7 +2,6 @@ package edu.java.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
-import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
@@ -12,13 +11,8 @@ import org.springframework.validation.annotation.Validated;
 public record ApplicationConfig(
     @NotNull
     Scheduler scheduler,
-    AccessType databaseAccessType,
-    Retry retry
+    AccessType databaseAccessType
 ) {
-    @Bean
-    public Retry retry() {
-        return retry;
-    }
 
     @Bean
     public Scheduler scheduler() {
@@ -26,11 +20,6 @@ public record ApplicationConfig(
     }
 
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
-    }
-
-    public record Retry(int maxAttempts, List<Class<? extends Throwable>> exceptions, Backoff backoff) {
-        public record Backoff(Long delay, Long maxDelay, Double multiplier) {
-        }
     }
 
     public enum AccessType {
